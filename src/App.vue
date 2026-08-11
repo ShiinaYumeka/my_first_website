c25aa7998349af0449714f6e003a88c01dd5f9a1
ShiinaYumeka <3249250682@qq.com>
2026-08-11 14:32:58 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div>
+    账号：<input v-model="userInfo.inputUsername"/> {{userInfo.username}} <br />
+    密码：<input v-model="userInfo.inputPassword"/> {{userInfo.password}} <br />
+    <button v-on:click="changePassword">登入</button>
+  </div>
+</template>
+
+
+<script lang="ts">
+
+  export default {
+    data(){
+      return {
+        userInfo:{
+          username : "SuperAdmin",
+          password: "SuperAdmin",
+          inputUsername : "",
+          inputPassword : "",
+        }
+      }
+    },
+    methods:{
+      changePassword(){
+        if(this.userInfo.password.length < 6) return "";
+        this.userInfo.username = this.userInfo.inputUsername.trim()
+        this.userInfo.password = this.userInfo.inputPassword.trim()
+      }
+    }
+  }
+</script>
+
+
+<style scoped>
+  .userInfo{
+    background: aqua;
+    color: #282828;
+  }
+
+</style>
